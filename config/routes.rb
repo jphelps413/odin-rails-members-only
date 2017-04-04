cdc129bdbf8ec223521ececbf2fcdfe7c7b4a15e
John Phelps <john@fractograf.com>
2017-04-04 17:08:17 -0400

    Reduced password min to 6 and cleaned up routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-
-  get 'static_pages/about'
-
-  root   'static_pages#home'
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/about'
 
   get    '/signup', to: 'users#new'
-  post   '/sugnup', to: 'users#create'
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
